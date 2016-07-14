ee0a021c14c5fe697eaf021b37373be51f7a264a
lilonglong <2453085960@qq.com>
2016-07-14 10:32:27 +0800

    add event

--- src/nju/base/ManageObject.js
+++ src/nju/base/ManageObject.js
@@ -1,9 +1,12 @@
+import EventEmitter from "wolfy87-eventemitter";
+
 export default class ManageObject
 {
         constructor(id =  null)
         {
             this._id = id;
             this._parent = null;
+            this._eventEmitter = new EventEmitter();
             this.init();
 
         }
@@ -21,5 +24,32 @@ export default class ManageObject
         get parent()
         {
             return this._parent;
+        }
+
+        get eventEmitter()
+        {
+
+        }
+
+        on(type, listener)
+        {
+            this.EventEmitter.on(type,listener);
+            return this;
+        }
+
+        off(type, listener)
+        {
+            this.EventEmitter.off(type,listener);
+            return this;
+        }
+
+        trigger(type, parameters = {})
+        {
+            const event = {
+                type,
+                parameters
+            };
+
+            this.eventEmitter.trigger(type, [ event ]);
         }
 }
