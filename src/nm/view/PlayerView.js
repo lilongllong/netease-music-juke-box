ba9010d9611492380d4d262d841bb3088cedf9d5
lilonglong <2453085960@qq.com>
2016-07-18 23:28:17 +0800

    Add playerView Layout settiongs

--- src/nm/view/PlayerView.js
+++ src/nm/view/PlayerView.js
@@ -7,6 +7,62 @@ export default class PlayerView extends View
         super.init();
         this._track = null;
         this.addStyleClass("nm-player-view");
+        this.initLayout();
+    }
+
+    initLayout()
+    {
+        this.initTrackBtnsView();
+        this.initTrackIconView();
+        this.initTrackPlayView();
+        this.initTrackShareView();
+        this.initTrackSettingView();
+    }
+
+    initTrackBtnsView()
+    {
+        this.$btnsView = $(`<div class="track-btns">
+                                    <span class="prev"></span>
+                                    <span class="play"></span>
+                                    <span class="next"></span>
+                                </div>`);
+        this.$container.append(this.$btnsView);
+    }
+
+    initTrackIconView()
+    {
+        this.$iconView = $(`<div class="track-icon"><span class="icon"></span></div>`);
+        this.$container.append(this.$iconView);
+    }
+
+    initTrackPlayView()
+    {
+        this.$playView = $(`<div class="track-play">
+                                    <div class="head">
+                                        <span id="track-name"></span>
+                                        <span id="track-artist"></span>
+                                    </div>
+                                    <div class="foot">
+                                        <div class="track-process"><div>
+                                        <span class="track-time"></span>
+                                    </div>
+                                </div>`);
+        this.$container.append(this.$playView);
+    }
+
+    initTrackShareView()
+    {
+        this.$shareView = $(`<div class="track-share">
+                                    <a class="favorite">收藏</a>
+                                    <a class="share">分享</a>
+                                </div>`)
+        this.$container.append(this.$shareView);
+    }
+
+    initTrackSettingView()
+    {
+        this.$settingView = $(`<div class="track-setting">相关设置</div>`);
+        this.$container.append(this.$settingView);
     }
 
     get track()
@@ -35,11 +91,4 @@ export default class PlayerView extends View
         }
     }
 
-    $createNewContent(track)
-    {
-        const $content = $(`<div class="track-operation"></div>
-                            <div class="track-process">${track.name}</div>
-                            <div class="track-like"></div>`);
-        return $content;
-    }
 }
